24f7ad6926a6c257ae965ff9f1a8b2bb675cee97
CelineCode <wang_nannan@outlook.com>
2024-09-19 22:58:09 -0400

    Add multiple items in the cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product)=>{
     </div>
 
     <div class="product-quantity-container">
-      <select>
+      <select class="js-quantity-selector-${product.id}">
         <option selected value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -59,6 +59,8 @@ document.querySelectorAll('.js-add-to-cart')
   .forEach((button)=>{
     button.addEventListener('click',()=>{
       const productId=button.dataset.productId;
+      const selectorElement=document.querySelector(`.js-quantity-selector-${productId}`);
+      const selectorValue=Number(selectorElement.value);
 
       let matchingItem;
 
@@ -69,11 +71,11 @@ document.querySelectorAll('.js-add-to-cart')
       });
 
       if(matchingItem){
-        matchingItem.quantity++;
+        matchingItem.quantity+=selectorValue;
       }else{
         cart.push({
           productId: productId,
-          quantity:1
+          quantity:selectorValue
         });
       }
 
